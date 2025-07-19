fdb756bf57a9ed67b78aee73146d4a25154effc3
MadiZhakenov <147735844+MadiZhakenov@users.noreply.github.com>
2025-07-19 16:27:11 +0500

    feat(users): add endpoint to get protocols

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -19,4 +19,16 @@ export class UsersController {
   getProfile(@Request() req) {
     return req.user;
   }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('protocols')
+  getProtocols(@Request() req) {
+    
+    console.log(`Пользователь ${req.user.email} запросил протоколы.`);
+
+    return [
+      { id: 1, name: 'Протокол собрания №1 от 01.06.2025', url: '/files/protocol1.pdf' },
+      { id: 2, name: 'Протокол собрания №2 от 01.07.2025', url: '/files/protocol2.pdf' },
+    ];
+  }
 }
