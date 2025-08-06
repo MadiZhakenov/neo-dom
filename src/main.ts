03050f563dbe190a48345ee8d96c43498f8fc1d6
MadiZhakenov <147735844+MadiZhakenov@users.noreply.github.com>
2025-08-06 17:24:42 +0500

    upd: main ts library

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
-import 'crypto';
+// 1. Импортируем 'crypto'
+import * as crypto from 'crypto';
+
+// 2. Явно "внедряем" его в глобальный объект Web Crypto API,
+// на который, скорее всего, рассчитывает @nestjs/typeorm.
+// Это называется "полифилл".
+if (!global.crypto) {
+  Object.defineProperty(global, 'crypto', {
+    value: {
+      randomUUID: () => crypto.randomUUID(),
+    },
+    configurable: true,
+  });
+}
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
