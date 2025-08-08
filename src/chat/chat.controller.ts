56cab248980521675b25cb9c862b36295c99ca5e
MadiZhakenov <147735844+MadiZhakenov@users.noreply.github.com>
2025-08-08 23:03:47 +0500

    upd: chat controller

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request, Header } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { ChatHistoryService } from './history/history.service';
 
@@ -11,6 +11,9 @@ export class ChatController {
    */
   @UseGuards(JwtAuthGuard)
   @Get('history')
+  @Header('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+  @Header('Pragma', 'no-cache')
+  @Header('Expires', '0')
   async getChatHistory(@Request() req) {
     const userId = req.user.userId;
     return this.chatHistoryService.getHistoryForUser(userId);
