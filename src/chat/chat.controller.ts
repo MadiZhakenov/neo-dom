8ccc7a3d023e4b67cc7aaabaee0d9138b37859ff
MadiZhakenov <147735844+MadiZhakenov@users.noreply.github.com>
2025-08-11 01:08:44 +0500

    upd: chat history cont

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,22 +1,31 @@
-// src\chat\chat.controller.ts
 import { Controller, Get, UseGuards, Request, Header } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { ChatHistoryService } from './history/history.service';
+import { ChatType } from './entities/chat-message.entity';
 
 @Controller('chat')
 export class ChatController {
   constructor(private readonly chatHistoryService: ChatHistoryService) {}
 
   /**
-   * Эндпоинт для получения полной истории чата текущего пользователя.
+   * Эндпоинт для получения истории "ИИ-Чат" (общий чат).
    */
   @UseGuards(JwtAuthGuard)
-  @Get('history')
-  @Header('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
-  @Header('Pragma', 'no-cache')
-  @Header('Expires', '0')
-  async getChatHistory(@Request() req) {
+  @Get('history/general')
+  @Header('Cache-Control', 'no-store') // Добавляем заголовок для надежности
+  async getGeneralChatHistory(@Request() req) {
     const userId = req.user.userId;
-    return this.chatHistoryService.getHistoryForUser(userId);
+    return this.chatHistoryService.getHistoryForUser(userId, ChatType.GENERAL);
+  }
+
+  /**
+   * Эндпоинт для получения истории "ИИ-Документы".
+   */
+  @UseGuards(JwtAuthGuard)
+  @Get('history/document')
+  @Header('Cache-Control', 'no-store')
+  async getDocumentChatHistory(@Request() req) {
+    const userId = req.user.userId;
+    return this.chatHistoryService.getHistoryForUser(userId, ChatType.DOCUMENT);
   }
 }
