cb1d59335791bb0379eca9338e840f6f4b40e3e6
MadiZhakenov <147735844+MadiZhakenov@users.noreply.github.com>
2025-08-11 10:54:31 +0500

    fix: new migrate

--- src/migrations/1754891549685-FinalSchema.ts
+++ src/migrations/1754891549685-FinalSchema.ts
@@ -1,14 +1,13 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class FinalSchema1754879688329 implements MigrationInterface {
-    name = 'FinalSchema1754879688329'
+export class FinalSchema1754891549685 implements MigrationInterface {
+    name = 'FinalSchema1754891549685'
 
-    public async up(queryRunner: QueryRunner): Promise<void> 
-    {
+    public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TYPE "public"."chat_message_role_enum" AS ENUM('user', 'model')`);
         await queryRunner.query(`CREATE TYPE "public"."chat_message_type_enum" AS ENUM('chat', 'document')`);
         await queryRunner.query(`CREATE TABLE "chat_message" ("id" SERIAL NOT NULL, "role" "public"."chat_message_role_enum" NOT NULL, "content" text NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "type" "public"."chat_message_type_enum" NOT NULL DEFAULT 'chat', "userId" integer, CONSTRAINT "PK_3cc0d85193aade457d3077dd06b" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "password_hash" character varying NOT NULL, "tariff" character varying NOT NULL DEFAULT 'Базовый', "last_generation_date" TIMESTAMP, "full_name" character varying, "phone" character varying, "role" character varying NOT NULL DEFAULT 'resident', "subscription_expires_at" TIMESTAMP, "password_reset_token" character varying, "password_reset_expires" TIMESTAMP, "password_change_required" boolean NOT NULL DEFAULT false, "generation_count" integer NOT NULL DEFAULT '0', CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "password_hash" character varying NOT NULL, "tariff" character varying NOT NULL DEFAULT 'Базовый', "last_generation_date" TIMESTAMP, "full_name" character varying, "phone" character varying, "role" character varying NOT NULL DEFAULT 'resident', "subscription_expires_at" TIMESTAMP, "password_reset_token" character varying, "password_reset_expires" TIMESTAMP, "password_change_required" boolean NOT NULL DEFAULT false, "generation_count" integer NOT NULL DEFAULT '0', "doc_chat_template" character varying, "doc_chat_request_id" character varying, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "apartments" ("id" SERIAL NOT NULL, "number" character varying NOT NULL, "address" character varying NOT NULL, "residentId" integer, CONSTRAINT "PK_f6058e85d6d715dbe22b72fe722" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "balances" ("id" SERIAL NOT NULL, "amount" numeric(10,2) NOT NULL, "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "apartmentId" integer, CONSTRAINT "PK_74904758e813e401abc3d4261c2" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "apartments" DROP COLUMN "residentId"`);
