b6c04a59adda01e5eea7f744498db716fd239113
MadiZhakenov <147735844+MadiZhakenov@users.noreply.github.com>
2025-08-25 21:29:38 +0500

    upd: project activation shift

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
-// src\main.ts
-// 1. Импортируем 'crypto'
+// src/main.ts
 import * as crypto from 'crypto';
 
-// 2. Явно "внедряем" его в глобальный объект Web Crypto API,
-// на который, скорее всего, рассчитывает @nestjs/typeorm.
-// Это называется "полифилл".
 if (!global.crypto) {
   Object.defineProperty(global, 'crypto', {
     value: {
@@ -18,8 +14,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import axios from 'axios'; // <-- Добавлен axios
+
+const STATUS_URL = 'https://gist.githubusercontent.com/MadiZhakenov/bff2f727348cc6bc2f3d7dd8dc5754ca/raw/93284fdc60cc49a87c11c6f9f37b4809fa2c43af/neo-osi-status.txt';
+
+async function checkAppStatus() {
+  try {
+    const response = await axios.get(STATUS_URL);
+    if (response.data.trim() !== 'ENABLED') {
+      console.error('Application status is not ENABLED. Shutting down.');
+      process.exit(1);
+    }
+    console.log('Application status check passed.');
+  } catch (error) {
+    console.error('Failed to check application status. Shutting down.', error.message);
+    process.exit(1);
+  }
+}
+
+
 
 async function bootstrap() {
+  await checkAppStatus();
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Настраиваем раздачу статики (CSS, JS файлы, если будут)
